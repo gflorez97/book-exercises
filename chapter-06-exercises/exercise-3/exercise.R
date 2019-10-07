c678f610c3a995c46eccacd87d74d8d2e239e7d4
gflorez97 <gflorez97@gmail.com>
2019-10-07 15:24:57 +0100

    Solve exercise 6.3

--- chapter-06-exercises/exercise-3/exercise.R
+++ chapter-06-exercises/exercise-3/exercise.R
@@ -3,7 +3,7 @@
 # Define a function `add_three` that takes a single argument and
 # returns a value 3 greater than the input
 add_three <- function(input){
-  return(input*3)
+  return(input+3)
 }
 
 # Create a variable `ten` that is the result of passing 7 to your `add_three` 
